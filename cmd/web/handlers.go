83337f059aab14eafa648b41104c623ab662cfd1
Ricardo Mendes <rmcs87@gmail.com>
2022-01-13 13:19:35 -0300

    Chapter 3.3

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,12 +3,11 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 	"strconv"
 )
 
-func home(writer http.ResponseWriter, request *http.Request) {
+func (app *application) home(writer http.ResponseWriter, request *http.Request) {
 	if request.URL.Path != "/" {
 		http.NotFound(writer, request)
 		return
@@ -22,21 +21,21 @@ func home(writer http.ResponseWriter, request *http.Request) {
 
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		log.Println(err.Error())
+		app.errorLog.Println(err.Error())
 		http.Error(writer, "Internal Server Error", 500)
 		return
 	}
 
 	err = ts.Execute(writer, nil)
 	if err != nil {
-		log.Println(err.Error())
+		app.errorLog.Println(err.Error())
 		http.Error(writer, "Internal Server Error", 500)
 	}
 
 	return
 }
 
-func createSnippet(writer http.ResponseWriter, request *http.Request) {
+func (app *application) createSnippet(writer http.ResponseWriter, request *http.Request) {
 	if request.Method != "POST" {
 		writer.Header().Set("Allow", "POST")
 		http.Error(writer, "Método não Permitido", 405)
@@ -47,7 +46,7 @@ func createSnippet(writer http.ResponseWriter, request *http.Request) {
 	writer.Write([]byte("Create a new snippet ..."))
 }
 
-func showSnippet(writer http.ResponseWriter, request *http.Request) {
+func (app *application) showSnippet(writer http.ResponseWriter, request *http.Request) {
 	id, err := strconv.Atoi(request.URL.Query().Get("id"))
 	if err != nil || id < 1 {
 		http.NotFound(writer, request)
