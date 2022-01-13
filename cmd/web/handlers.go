0d02b07998e2cb1c6389cc6f9a26857d0fcf7f75
Ricardo Mendes <rmcs87@gmail.com>
2022-01-13 13:35:19 -0300

    Chapter 3.4

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -9,7 +9,7 @@ import (
 
 func (app *application) home(writer http.ResponseWriter, request *http.Request) {
 	if request.URL.Path != "/" {
-		http.NotFound(writer, request)
+		app.notFound(writer)
 		return
 	}
 
@@ -21,15 +21,13 @@ func (app *application) home(writer http.ResponseWriter, request *http.Request) 
 
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		app.errorLog.Println(err.Error())
-		http.Error(writer, "Internal Server Error", 500)
+		app.serverError(writer, err)
 		return
 	}
 
 	err = ts.Execute(writer, nil)
 	if err != nil {
-		app.errorLog.Println(err.Error())
-		http.Error(writer, "Internal Server Error", 500)
+		app.serverError(writer, err)
 	}
 
 	return
@@ -38,7 +36,7 @@ func (app *application) home(writer http.ResponseWriter, request *http.Request) 
 func (app *application) createSnippet(writer http.ResponseWriter, request *http.Request) {
 	if request.Method != "POST" {
 		writer.Header().Set("Allow", "POST")
-		http.Error(writer, "Método não Permitido", 405)
+		app.clientError(writer, http.StatusMethodNotAllowed)
 
 		return
 	}
@@ -49,7 +47,7 @@ func (app *application) createSnippet(writer http.ResponseWriter, request *http.
 func (app *application) showSnippet(writer http.ResponseWriter, request *http.Request) {
 	id, err := strconv.Atoi(request.URL.Query().Get("id"))
 	if err != nil || id < 1 {
-		http.NotFound(writer, request)
+		app.notFound(writer)
 		return
 	}
 	fmt.Fprintf(writer, "Exibindo Snippet de ID: %d", id)
