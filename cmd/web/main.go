6d763a97cae96465d45ca1b5d33a44dd3e85deab
Ricardo Mendes <rmcs87@gmail.com>
2022-01-14 09:33:18 -0300

    Chapter 4.3 Added SQL Connection

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql" // New import
 )
 
 type Config struct {
@@ -16,16 +19,28 @@ type application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
 	cfg      *Config
+	db       *sql.DB
 }
 
 func main() {
 	cfg := new(Config)
 	flag.StringVar(&cfg.Addr, "addr", ":8080", "HTTP network address")
 	flag.StringVar(&cfg.StaticDir, "static-dir", ".\\ui\\static\\", "Path to static Files")
+
+	dsn := flag.String("dsn", "root:123456@/snippetbox?parseTime=true", "Mysql Connection String")
+
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDB(*dsn)
+
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	defer db.Close()
 
 	app := &application{
 		errorLog: errorLog,
@@ -41,6 +56,17 @@ func main() {
 		Handler:  app.routes(),
 	}
 
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
