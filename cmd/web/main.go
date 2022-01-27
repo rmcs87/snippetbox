ccca67055fcf2de56b265ec6f5449dfa61324b26
Ricardo Mendes <rmcs87@gmail.com>
2022-01-27 10:14:36 -0300

    Chapter 11.2 - User Model

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -28,6 +28,7 @@ type application struct {
 	snippets      *mysql.SnippetModel
 	templateCache map[string]*template.Template
 	session       *sessions.Session
+	users         *mysql.UserModel
 }
 
 func main() {
@@ -66,6 +67,7 @@ func main() {
 		snippets:      &mysql.SnippetModel{DB: db},
 		templateCache: templateCache,
 		session:       session,
+		users:         &mysql.UserModel{DB: db},
 	}
 
 	tlsConfig := &tls.Config{
